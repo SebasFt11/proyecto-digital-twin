c79da3515c933fb5a0a886d6683c5de21c4187ce
SebasFt11 <143882555+SebasFt11@users.noreply.github.com>
2023-11-22 23:05:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,33 +6,6 @@ import plotly.graph_objs as go
 
 from frontend.derecho.derecho import *  # Importar la variable "derecho" de la carpeta del frontend
 from frontend.izquierdo.izquierdo import *    # Importar la variable "izquierdo" de la carpeta del frontend
-from frontend.inferior.inferior import inferior    # Importar la variable "inferior" de la carpeta del frontend
-
-
-# Crear el diseño de la página web
-layout = dbc.Container([  # Se crea un contenedor de Dash que señala los elementos que contendrán la página.
-    dbc.Row([  # Crear una fila para el encabezado
-        dbc.Col(html.H1('Monitoreo de Asentamiento Tuneladora',style = {'background-color': '#188EBE', 'color': 'white','text-align': 'center'}), md = 12),  # Crear una columna que contiene un título de nivel 1 y su estilo
-    ]),
-    dbc.Row([  # Crear una fila para los contenidos principales
-        dbc.Col(izquierdo, md=5, style = {'background-color' : '#E9F3FF'}),  # Crear una columna que contiene el componente "izquierdo", que se importa de otra carpeta donde ya esta definida y el estilo de esta
-        dbc.Col(derecho, md=7,className="d-flex flex-wrap justify-content-center", style = {'background-color' : '#C9E2FF'}),  # Crear una columna que contiene el componente "derecho", que se importa de otra carpeta donde ya esta definida y el estilo de esta
-    ]),
-    html.H2("Gráfica Asentamiento Tuneladora", style={'text-align': 'center'}),
-    html.Hr(),
-    html.H4(id='distancia-actual', style={'text-align': 'center'}),
-    dcc.Graph(id='asentamiento'),
-    dcc.Interval(
-        id='interval-component',
-        interval=1 ,  # en milisegundos, actualiza cada 1 segundo
-        n_intervals=0
-    ),
-    html.Div(id='alerta-texto', style={'text-align': 'center', 'margin-top': '10px'})
-])
-
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP]) # Crear una instancia de la aplicación Dash
-server = app.server
-app.layout = layout
 
 #Base de datos
 from pymongo.mongo_client import MongoClient
@@ -51,6 +24,30 @@ result = 0
 
 # Declarar data_dist fuera de la función para evitar el UnboundLocalError
 data_dist = []
+
+# Crear el diseño de la página web
+layout = dbc.Container([  # Se crea un contenedor de Dash que señala los elementos que contendrán la página.
+    dbc.Row([  # Crear una fila para el encabezado
+        dbc.Col(html.H1('Monitoreo de Asentamiento Tuneladora',style = {'background-color': '#188EBE', 'color': 'white','text-align': 'center'}), md = 12),  # Crear una columna que contiene un título de nivel 1 y su estilo
+    ]),
+    dbc.Row([  # Crear una fila para los contenidos principales
+        dbc.Col(izquierdo, md=5, style = {'background-color' : '#E9F3FF'}),  # Crear una columna que contiene el componente "izquierdo", que se importa de otra carpeta donde ya esta definida y el estilo de esta
+        dbc.Col(derecho, md=7,className="d-flex flex-wrap justify-content-center", style = {'background-color' : '#C9E2FF'}),  # Crear una columna que contiene el componente "derecho", que se importa de otra carpeta donde ya esta definida y el estilo de esta
+    ]),
+    html.H2("Gráfica Asentamiento Tuneladora", style={'text-align': 'center'}),
+    html.Hr(),
+    html.H4(id='distancia-actual', style={'text-align': 'center'}),
+    dcc.Graph(id='asentamiento'),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*500 ,  # en milisegundos, actualiza cada 1 segundo
+        n_intervals=0
+    ),
+    html.Div(id='alerta-texto', style={'text-align': 'center', 'margin-top': '10px'})
+])
+
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP]) # Crear una instancia de la aplicación Dash
+app.layout = layout
 
 
 @app.callback(
@@ -75,23 +72,21 @@ def consultar(n):
         type="line",
         x0=0,
         x1=len(data_dist),
-        y0=10,
-        y1=10,
+        y0=1600,
+        y1=1600,
         line=dict(color="red", width=2),
     )
     
     # Agregar un texto según la condición
-    if distancia >= 10:
+    if distancia >= 1600:
         alerta_texto = html.Span("ALERTA", style={'color': 'red', 'font-size': '24px'})
     else:
         alerta_texto = html.Span("VAMOS BIEN", style={'color': 'green', 'font-size': '24px'})
-    
     
     # Formatear la distancia para mostrarla en el H1
     distancia_texto = f"El asentamiento fue: {distancia} cm"
     
     return fig, distancia_texto,alerta_texto
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
